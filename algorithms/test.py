0b014ec87aab8e41f777fe4ce729f4ed771f258d
Bruno Petrus <bruno.petrus@live.com>
2019-12-31 00:17:27 +0100

    Time tests for various algos.

--- algorithms/test.py
+++ algorithms/test.py
@@ -1,8 +1,30 @@
 import heapsort as hs
+import quicksort as qs
+import insertionsort as ins
 import random
+import time
 
-n = random.randrange(0, 1000000)
-arr = [x for x in range(n)]
+start = time.time()
+n = random.randrange(0, 100000)
+arr = [random.randrange(0, 100000) for x in range(n)]
+arr1 = arr.copy()  # Heapsort
+arr2 = arr.copy()  # Quicksort with random pivot
+arr3 = arr.copy()  # Quicksort with fixed pivot
+arr4 = arr.copy()  # Insertion sort
+print("[~] Generating {:} numbers took {:} s".format(n, time.time()-start))
 
-hs.heapsort(arr)
-#print(arr)
+start = time.time()
+hs.heapsort(arr1)
+print("[~] Heapsort took {:} s".format(time.time()-start))
+
+start = time.time()
+qs.quicksort(arr2, 0, len(arr2)-1, True)
+print("[~] Quicksort with random pivot took {:} s".format(time.time()-start))
+
+start = time.time()
+qs.quicksort(arr3, 0, len(arr2)-1)
+print("[~] Quicksort took {:} s".format(time.time()-start))
+
+start = time.time()
+ins.insertionsort(arr4)
+print("[~] Insertion sort took {:} s".format(time.time()-start))
