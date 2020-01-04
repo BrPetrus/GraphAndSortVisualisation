eeee655c8de16d88b213bb415817e40caab13170
Bruno Petrus <bruno.petrus@live.com>
2020-01-04 14:19:04 +0100

    Heapsort is no longer a thread sublass. Instead it returns a Queue object with all swaps.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import tkinter
-canvas = tkinter.Canvas(bg='black', height=1000, width=1000)
-canvas.pack()
-canvas.mainloop()
+# import tkinter
+# canvas = tkinter.Canvas(bg='black', height=1000, width=1000)
+# canvas.pack()
+# canvas.mainloop()
+import visualise_sort
+from algorithms import heapsort
+
+vs = visualise_sort.VisualiseSort(heapsort.HeapSort)
+vs.run()
+
+
--- visualise_sort.py
+++ visualise_sort.py
@@ -1,4 +1,38 @@
-class VisualiseSort:
-    def __init__(self, canvas, sorting_algorithm):
-        pass 
+import threading
+import random
 
+
+class VisualiseSort():
+    def __init__(self, sorting_algorithm_class):
+        self.threadLock = threading.Lock()
+        self.sorting_algorithm_class = sorting_algorithm_class
+
+        # Generate array
+        self.arr = [i for i in range(10)]
+
+        # Shuffle array
+        N = 5
+        for i in range(N):
+            j, k = random.randint(0, 9), random.randint(0, 9)
+            self.arr[j], self.arr[k] = self.arr[k], self.arr[j]
+
+    def run(self):
+        # Run sort function
+        self.thread = self.sorting_algorithm_class(self.threadLock, self.arr)
+        self.thread.start()
+        print(type(self.thread))
+
+        while self.thread.isAlive():
+            # self.threadLock.acquire()
+            # self.thread.join()
+            self.threadLock.acquire()
+            print('Drawin')
+            self.thread.release()
+            print(self.thread.array)
+
+
+        print('[~] Done sorting')
+        # print(self.arr)
+
+    def draw(self):
+        pass
